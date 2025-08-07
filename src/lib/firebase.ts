809545bc56e38abfc9355ffa299f89fdf09c372a
Global Pearl Ventures <info.globalpearlventures@gmail.com>
2025-08-07 06:39:46 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  projectId: 'project-pulse-17wzf',
-  appId: '1:906680989803:web:a2fc77dd9c0806ece6592e',
-  storageBucket: 'project-pulse-17wzf.firebasestorage.app',
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'project-pulse-17wzf.firebaseapp.com',
-  messagingSenderId: '906680989803',
-  measurementId: 'YOUR_MEASUREMENT_ID',
+  apiKey: "AIzaSyC9UTcTKTzX0mesZg6HG4Mr0ELWFXtnbr0",
+  authDomain: "project-pulse-17wzf.firebaseapp.com",
+  projectId: "project-pulse-17wzf",
+  storageBucket: "project-pulse-17wzf.firebasestorage.app",
+  messagingSenderId: "906680989803",
+  appId: "1:906680989803:web:a2fc77dd9c0806ece6592e"
 };
 
 // Initialize Firebase
