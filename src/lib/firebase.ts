16d1e3b9bc39320c20eb6fd9ec4116d3206a67d6
Global Pearl Ventures <info.globalpearlventures@gmail.com>
2025-08-07 06:42:53 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,13 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC9UTcTKTzX0mesZg6HG4Mr0ELWFXtnbr0",
-  authDomain: "project-pulse-17wzf.firebaseapp.com",
-  projectId: "project-pulse-17wzf",
-  storageBucket: "project-pulse-17wzf.firebasestorage.app",
-  messagingSenderId: "906680989803",
-  appId: "1:906680989803:web:a2fc77dd9c0806ece6592e"
+  "projectId": "project-pulse-17wzf",
+  "appId": "1:906680989803:web:a2fc77dd9c0806ece6592e",
+  "storageBucket": "project-pulse-17wzf.firebasestorage.app",
+  "apiKey": "AIzaSyC9UTcTKTzX0mesZg6HG4Mr0ELWFXtnbr0",
+  "authDomain": "project-pulse-17wzf.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "906680989803"
 };
 
 // Initialize Firebase
